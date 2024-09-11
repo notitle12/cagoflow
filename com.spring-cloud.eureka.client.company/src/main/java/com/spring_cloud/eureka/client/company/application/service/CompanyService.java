b4f3fa87e8cc2ee2b3a2a59958340b8d57bf7d1b
kmc198989 <kmc198989@naver.com>
2024-09-11 13:55:26 +0900

    refactor: company 생성 api 수정

--- com.spring-cloud.eureka.client.company/src/main/java/com/spring_cloud/eureka/client/company/application/service/CompanyService.java
+++ com.spring-cloud.eureka.client.company/src/main/java/com/spring_cloud/eureka/client/company/application/service/CompanyService.java
@@ -2,6 +2,8 @@ package com.spring_cloud.eureka.client.company.application.service;
 
 
 import com.spring_cloud.eureka.client.company.domain.service.CompanyDomainService;
+import com.spring_cloud.eureka.client.company.infrastructure.client.HubClient;
+import com.spring_cloud.eureka.client.company.infrastructure.client.HubResponse;
 import com.spring_cloud.eureka.client.company.presentation.request.CompanyRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,8 +13,17 @@ import org.springframework.stereotype.Service;
 public class CompanyService {
 
     private final CompanyDomainService companyDomainService;
+    private final HubClient hubClient;
+
 
     public void createCompany(CompanyRequest companyRequest) {
+        HubResponse hubResponse = hubClient.getHubById(companyRequest.getHubId());
+        if (hubResponse == null) {
+            throw new IllegalArgumentException("유효하지 않은 허브 ID 입니다.");
+        }
+
+        // userId 검증 로직 추가하기
+
         companyDomainService.createCompany(
                 companyRequest.getCompanyName(),
                 companyRequest.getHubId(),
