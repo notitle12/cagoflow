a9a7c17e179d1fb9f6edbe5ec182fa00aa974cdd
notitle12 <notitle1215@gmail.com>
2024-09-12 13:09:23 +0900

    refactor: gateway LocalJwtAuthenticationFilter 코드 리펙토링

--- com.spring-cloud.eureka.client.gateway/src/main/java/com/spring_cloud/eureka/client/gateway/LocalJwtAuthenticationFilter.java
+++ com.spring-cloud.eureka.client.gateway/src/main/java/com/spring_cloud/eureka/client/gateway/LocalJwtAuthenticationFilter.java
@@ -3,7 +3,6 @@ package com.spring_cloud.eureka.client.gateway;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -14,7 +13,6 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import javax.crypto.SecretKey;
-import java.util.Base64;
 
 @Slf4j
 @Component
@@ -25,97 +23,71 @@ public class LocalJwtAuthenticationFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        try{
-            String path = exchange.getRequest().getURI().getPath();
-            // 로그인 회원가입은 토큰 검증 없이 진행
-            if (authorizationPassRequest(path)) {
-                return chain.filter(exchange);
-            }
-            String token = extractToken(exchange);
+        String path = exchange.getRequest().getURI().getPath();
 
-            if (token == null || !validateToken(token, exchange)) {
-                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                return exchange.getResponse().setComplete();
-            }
-
+        // 로그인 및 회원가입 경로는 토큰 검증을 통과합니다.
+        if (isAuthorizationPassRequest(path)) {
             return chain.filter(exchange);
-        } catch (ExpiredJwtException | MalformedJwtException | UnsupportedJwtException
-                 | SecurityException | IllegalArgumentException e) {
-            log.error("JWT 검증 오류: ", e);
+        }
+
+        String token = extractToken(exchange);
+        if (token == null || !validateToken(token, exchange)) {
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
             return exchange.getResponse().setComplete();
         }
+
+        return chain.filter(exchange);
     }
 
     private String extractToken(ServerWebExchange exchange) {
         String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
-        if (authHeader != null) {
-            System.out.println("Authorization Header: " + authHeader);
-        }
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             return authHeader.substring(7);
         }
         return null;
     }
 
-    /**
-     * 토큰 검증 및 토큰의 정보를 추출
-     */
     private boolean validateToken(String token, ServerWebExchange exchange) {
-//        Claims claims = getClaimsFromToken(token);
         try {
-            // SecretKey를 올바르게 설정합니다.
-            SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
+            SecretKey key = getSecretKey();
+            Claims claims = getClaimsFromToken(token, key);
 
-            // 토큰에서 Claims를 추출합니다.
-            Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
-
-            // Claims에서 userId를 가져옵니다.
             Integer userId = claims.get("userId", Integer.class);
             if (userId == null) {
-                throw new JwtException("UserId not found in token claims");
+                throw new JwtException("토큰 클레임에서 UserId를 찾을 수 없습니다.");
             }
 
-            // 요청 헤더에 사용자 정보를 추가합니다.
-            exchange.getRequest().mutate()
-                    .header("X-User-Id", userId.toString())
-                    .header("X-Role", claims.get("auth").toString())
-                    .build();
-
+            addHeadersToRequest(exchange, userId, claims);
             log.info(userId + " " + claims.get("auth") + " 인가 통과");
+
             return true;
-        } catch (ExpiredJwtException e) {
-            log.error("토큰이 만료되었습니다", e);
-            return false;
-        } catch (MalformedJwtException e) {
-            log.error("잘못된 JWT 서명입니다", e);
-            return false;
-        } catch (UnsupportedJwtException e) {
-            log.error("지원하지 않는 JWT입니다", e);
-            return false;
-        } catch (SecurityException e) {
-            log.error("JWT 서명에 문제가 있습니다", e);
-            return false;
-        } catch (IllegalArgumentException e) {
-            log.error("JWT가 잘못되었습니다", e);
-            return false;
-        }
-    }
-
-    private boolean authorizationPassRequest(String path) {
-        return path.startsWith("/auth/login") || path.startsWith("/auth/sign-up");
-    }
-
-    /**
-     * 토큰 정보 추출
-     */
-    public Claims getUserInfoFromToken(String token, SecretKey key) {
-        try {
-            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
         } catch (JwtException e) {
-            // 로그 추가
-            log.error("Error parsing JWT claims", e);
-            throw e;
+            log.error("JWT 검증 오류: ", e);
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return false;
         }
+    }
+
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
+    }
+
+    private Claims getClaimsFromToken(String token, SecretKey key) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private void addHeadersToRequest(ServerWebExchange exchange, Integer userId, Claims claims) {
+        exchange.getRequest().mutate()
+                .header("X-User-Id", userId.toString())
+                .header("X-Role", claims.get("auth").toString())
+                .build();
+    }
+
+    private boolean isAuthorizationPassRequest(String path) {
+        return path.startsWith("/auth/login") || path.startsWith("/auth/sign-up");
     }
 }
