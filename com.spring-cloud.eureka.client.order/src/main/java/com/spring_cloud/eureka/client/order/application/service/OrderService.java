886e2f343640d360fafe62f1881af9c99d959a8f
DESKTOP-N8PDVAS\WeSeongGu <fkskdldh@naver.com>
2024-09-12 20:46:33 +0900

    test

--- com.spring-cloud.eureka.client.order/src/main/java/com/spring_cloud/eureka/client/order/application/service/OrderService.java
+++ com.spring-cloud.eureka.client.order/src/main/java/com/spring_cloud/eureka/client/order/application/service/OrderService.java
@@ -3,10 +3,8 @@ package com.spring_cloud.eureka.client.order.application.service;
 import com.spring_cloud.eureka.client.order.domain.repository.OrderRepository;
 import com.spring_cloud.eureka.client.order.presentaion.dtos.RequestOrderDto;
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
+
 import org.example.HubInformationFromCompanyDTO;
-=======
->>>>>>> cargoflow/feature/order
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +14,6 @@ public class OrderService {
     private final OrderRepository orderRepository;
 
     public void createOrderService(RequestOrderDto orderDto) {
-
+        HubInformationFromCompanyDTO a;
     }
 }
