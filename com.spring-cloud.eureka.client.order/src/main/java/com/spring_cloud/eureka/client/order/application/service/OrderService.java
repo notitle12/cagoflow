053b080fbd3c77106020e2d59c7ad5d1de240231
DESKTOP-N8PDVAS\WeSeongGu <fkskdldh@naver.com>
2024-09-12 20:39:21 +0900

    fix order

--- com.spring-cloud.eureka.client.order/src/main/java/com/spring_cloud/eureka/client/order/application/service/OrderService.java
+++ com.spring-cloud.eureka.client.order/src/main/java/com/spring_cloud/eureka/client/order/application/service/OrderService.java
@@ -3,6 +3,7 @@ package com.spring_cloud.eureka.client.order.application.service;
 import com.spring_cloud.eureka.client.order.domain.repository.OrderRepository;
 import com.spring_cloud.eureka.client.order.presentaion.dtos.RequestOrderDto;
 import lombok.RequiredArgsConstructor;
+import org.example.HubInformationFromCompanyDTO;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,5 @@ public class OrderService {
     private final OrderRepository orderRepository;
 
     public void createOrderService(RequestOrderDto orderDto) {
-        Hub
     }
 }
