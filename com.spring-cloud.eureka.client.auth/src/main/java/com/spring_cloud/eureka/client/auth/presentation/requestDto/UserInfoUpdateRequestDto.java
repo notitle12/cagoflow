e5d2fecce8ce4a710d9f1cc069c78fc227460c7f
notitle12 <notitle1215@gmail.com>
2024-09-19 22:18:26 +0900

    feat: 사용자 관련 기능 전역예외설정

--- com.spring-cloud.eureka.client.auth/src/main/java/com/spring_cloud/eureka/client/auth/presentation/requestDto/UserInfoUpdateRequestDto.java
+++ com.spring-cloud.eureka.client.auth/src/main/java/com/spring_cloud/eureka/client/auth/presentation/requestDto/UserInfoUpdateRequestDto.java
@@ -7,14 +7,10 @@ import lombok.Data;
 
 @Data
 public class UserInfoUpdateRequestDto {
-//    @NotBlank
-//    @Pattern(regexp = "^[a-z0-9]{4,10}$", //최소 4자 이상, 10자 이하이며 알파벳 소문자(a~z), 숫자(0~9)로 구성
-//            message = "아이디는 최소 4자 이상, 10자 이하이며 알파벳 소문자(a~z), 숫자(0~9)로 구성되어야 합니다.")
-//    private String username;
 
     @NotBlank
-    @Size(min = 8, max = 15, message = "Password must be between 8 and 15 characters long")
+    @Size(min = 8, max = 15, message = "비밀번호는 최소 8자 이상, 15자 이하여야 합니다.")
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]*$", //최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자
-            message = "비밀번호는 최소 8자 이상, 15자 이하이며, 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다.")
+            message = "비밀번호는 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다.")
     private String password;
 }
